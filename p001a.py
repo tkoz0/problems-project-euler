bf66202a427d5b40c9a9a846e5ff98007265e4e0
Your Name <you@example.com>
2018-03-16 15:44:57 -0400

    wrote a real quick easy p001 solution

---
+++ p001a.py
@@ -0,0 +1,21 @@
+# The trivial solution is a loop with a condition for summing
+
+sum_ = 0
+for i in range(1000):
+    if i % 3 == 0 or i % 5 == 0:
+        sum_ += i
+print(sum_)
+
+# Here is a 1 line solution for that
+print(sum(x for x in range(1000) if x % 3 == 0 or x % 5 == 0))
+
+# Using summation formula 1+2+...+n = n(n+1)/2
+# (3+6+...) + (5+10+...) - (15+30+...)
+# Sum required multiples of 3 and 5 and subtract the ones summed twice
+intsum = lambda x : x * (x + 1) // 2 # 1+2+...+x
+max_ = 999
+threes = 3 * intsum(max_ // 3)
+fives = 5 * intsum(max_ // 5)
+fifteens = 15 * intsum(max_ // 15)
+sum_ = threes + fives - fifteens
+print(sum_)
