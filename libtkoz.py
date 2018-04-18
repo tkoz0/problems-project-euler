129b91c38a087b5d9cd638b908c50c12cc398f9b
Your Name <you@example.com>
2018-04-18 19:47:14 -0400

    library fixing and testing functions

--- libtkoz.py
+++ libtkoz.py
@@ -14,7 +14,7 @@ def palindrome(x):
 
 # makes a list of primes from 2 to n (inclusive)
 # slow, tests every prime
-def list_primes(n):
+def list_primes1(n):
     primes = []
     for p in range(2, n+1):
         if prime(p):
@@ -22,8 +22,40 @@ def list_primes(n):
     return primes
 
 def gcd_euclid(m, n):
-    assert m > n
+    assert m >= n > 0
     while m % n != 0:
         m, n = n, m % n
     return n
+
+def is_square(n):
+    assert n >= 0
+    s = int(math.sqrt(n))
+    return s**2 == n
+
+# some tests for these functions to check that they work properly
+if __name__ == '__main__':
+    assert not prime(1) and prime(2) and prime(3)
+    assert not prime(4) and prime(5) and not prime(6) and prime(7)
+    assert not prime(8) and not prime(9) and not prime(10)
+    assert prime(11) and prime(13) and prime(17) and prime(19)
+    assert prime(163)
+    assert not prime(289) and not prime(561)
+    assert not prime(1000013)
+    assert prime(1000000007)
+    assert palindrome(2) and palindrome(7) and palindrome(33)
+    assert not palindrome(23) and not palindrome(37)
+    assert palindrome(15751) and palindrome(843348)
+    assert not palindrome(15752) and not palindrome(832348)
+    assert list_primes1(1) == [] and list_primes1(2) == [2]
+    assert list_primes1(20) == [2, 3, 5, 7, 11, 13, 17, 19]
+    assert len(list_primes1(104742)) == 10000
+    assert gcd_euclid(2, 1) == 1
+    assert gcd_euclid(73, 73) == 73
+    assert gcd_euclid(2250, 1050) == 150
+    assert gcd_euclid(72, 56) == 8 and gcd_euclid(96, 72) == 24
+    assert is_square(0) and is_square(1)
+    assert is_square(64) and is_square(289)
+    assert not is_square(65) and not is_square(288)
+    assert is_square(4294967296) and not is_square(4294967295)
+    print('passed all tests')
 
--- p005b.py
+++ p005b.py
@@ -4,7 +4,7 @@ maxdiv = 20
 
 # build with the factorization
 num = 1
-for p in lib.list_primes(maxdiv):
+for p in lib.list_primes1(maxdiv):
     val = p
     while val <= maxdiv: # multiply by p^a, such that p^a <= max
         num *= p
