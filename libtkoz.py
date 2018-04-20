6184b7b8795f05c8b9392a8c407c89e0ae5d5110
Your Name <you@example.com>
2018-04-20 18:00:59 -0400

    added library functions for counting divisors

--- libtkoz.py
+++ libtkoz.py
@@ -1,6 +1,6 @@
 import math
 
-def prime(n):
+def prime(n): # requires sqrt(n) time
     if n < 2: return False;
     if n == 2 or n == 3: return True;
     if n % 2 == 0: return False;
@@ -14,7 +14,7 @@ def palindrome(x):
 
 # makes a list of primes from 2 to n (inclusive)
 # slow, tests every prime
-def list_primes1(n):
+def list_primes1(n): # takes n*sqrt(n) time
     primes = []
     for p in range(2, n+1):
         if prime(p):
@@ -32,6 +32,39 @@ def is_square(n):
     s = int(math.sqrt(n))
     return s**2 == n
 
+# slow loop for counting divisors, finds each factor
+def divisors1(n): # requires sqrt(n) time
+    assert n > 0
+    count = 2 # 1 and n
+    for d in range(2, int(math.sqrt(n))+1):
+        if n % d == 0:
+            count += 2
+    if is_square(n): # its square was root counted twice
+        count -= 1
+    return count
+
+# count divisors by factoring
+def divisors2(n):
+    assert n > 0
+    total = 1 # will be multiplied by counting prime factors
+    while n % 2 == 0: # factors of 2
+        total += 1
+        n //= 2
+    d = 3 # loop over possible odd factors
+    while d * d <= n: # d < square root, n will get smaller so calculate this
+        if n % d != 0:
+            d += 2
+            continue
+        count = 1
+        n //= d
+        while n % d == 0:
+            n //= d
+            count += 1
+        total *= count + 1
+        d += 2
+    if n == 1: return total # all factors divided out
+    else: return total * 2 # remaining value is a prime factor
+
 # some tests for these functions to check that they work properly
 if __name__ == '__main__':
     assert not prime(1) and prime(2) and prime(3)
@@ -42,20 +75,41 @@ if __name__ == '__main__':
     assert not prime(289) and not prime(561)
     assert not prime(1000013)
     assert prime(1000000007)
+    #
     assert palindrome(2) and palindrome(7) and palindrome(33)
     assert not palindrome(23) and not palindrome(37)
     assert palindrome(15751) and palindrome(843348)
     assert not palindrome(15752) and not palindrome(832348)
+    #
     assert list_primes1(1) == [] and list_primes1(2) == [2]
     assert list_primes1(20) == [2, 3, 5, 7, 11, 13, 17, 19]
     assert len(list_primes1(104742)) == 10000
+    #
     assert gcd_euclid(2, 1) == 1
     assert gcd_euclid(73, 73) == 73
     assert gcd_euclid(2250, 1050) == 150
     assert gcd_euclid(72, 56) == 8 and gcd_euclid(96, 72) == 24
+    #
     assert is_square(0) and is_square(1)
     assert is_square(64) and is_square(289)
     assert not is_square(65) and not is_square(288)
     assert is_square(4294967296) and not is_square(4294967295)
+    #
+    for p in list_primes1(200):
+        assert divisors1(p) == 2
+    assert divisors1(72) == 12
+    assert divisors1(9) == 3 and divisors1(289) == 3
+    assert divisors1(64) == 7 and divisors1(4294967296) == 33
+    assert divisors1(2*2*5*7*11*11*11) == 3 * 2 * 2 * 4
+    assert divisors1(1) == 1
+    #
+    for p in list_primes1(200):
+        assert divisors2(p) == 2
+    assert divisors2(72) == 12
+    assert divisors2(9) == 3 and divisors2(289) == 3
+    assert divisors2(64) == 7 and divisors2(4294967296) == 33
+    assert divisors2(2*2*5*7*11*11*11) == 3 * 2 * 2 * 4
+    assert divisors2(1) == 1
+    #
     print('passed all tests')
 
