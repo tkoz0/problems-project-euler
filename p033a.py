aba42dccfdde0b17e19cbb260e8bfbd9e330e422
Your Name <you@example.com>
2018-05-25 23:26:14 -0400

    p033 second solution

--- p033a.py
+++ p033a.py
@@ -27,3 +27,27 @@ gcd = lib.gcd_euclid(product[1], product[0])
 print(': simplified', (product[0] // gcd, product[1] // gcd))
 print(product[1] // gcd)
 
+# working out possibilities
+# 4 solution forms, i is cancellation digit, n and d are what will remain
+# conditions: 1<=n<d<=9, 1<=i<=9
+# (10i+n)/(10i+d)=n/d --> rearrange to n=d which cant be true
+# (10n+i)/(10d+i)=n/d --> also rearranges to n=d
+# (10i+n)/(10d+i)=n/d --> 10id+nd=10dn+in --> 10id-in=9nd
+# --> 9id+i(d-n)=9nd --> i(d-n)=9d(n-i) --> n-i=i/9-(in)/(9d)
+# d-n>0 so we must have n-i>0
+# next we get n-i>=1 and i/9<=1, since next term >0, right side <1 so not true
+# finally (10n+i)/(10i+d)=n/d --> 9nd=10in-id --> 9n(d-i)=i(n-d)
+# n-d<0 so d-i<0 which means i>d so n<d<i
+# loop for n,d,i and test equality of d(10n+i)=n(10i+d)
+product = (1, 1)
+for n in range(1, 10):
+    for d in range(n+1, 10):
+        for i in range(d+1, 10):
+            if d * (10*n + i) == n * (10*i + d):
+                print(':', 10*n+i, '/', 10*i+d, '=', n, '/', d)
+                product = (product[0] * n, product[1] * d)
+print(': product is', product)
+gcd = lib.gcd_euclid(product[1], product[0])
+print(': simplified', (product[0] // gcd, product[1] // gcd))
+print(product[1] // gcd)
+
