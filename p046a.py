5d14b58c869cae4fe70dc4e3127596c9f4bf8a83
Your Name <you@example.com>
2018-05-29 22:59:04 -0400

    p046 solution

---
+++ p046a.py
@@ -0,0 +1,22 @@
+import libtkoz as lib
+import math
+
+# brute force with prime cache
+cache = set(lib.list_primes2(10000))
+cachemax = max(cache)
+def is_prime(n):
+    if n <= cachemax: return n in cache
+    return lib.prime(n)
+
+n = 9
+while True: # loop to test composites
+    if not is_prime(n):
+        satisfiesconjecture = False
+        for s in range(1, int(math.sqrt(n//2))+1): # find a square that works
+            if is_prime(n - 2*s*s):
+                satisfiesconjecture = True
+                break
+        if not satisfiesconjecture:
+            print(n)
+            break
+    n += 2
