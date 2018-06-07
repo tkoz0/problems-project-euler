0db0208743f845575462a7b1f19291d43085b3b7
Your Name <you@example.com>
2018-06-07 11:20:09 -0400

    p078 use modulus instead to save memory

--- p078a.py
+++ p078a.py
@@ -2,7 +2,7 @@
 divby = 1000000
 
 # dynamic programming approach, though n^2 it scales poorly, solution is large
-# takes ~15min (i5-2540m)
+# takes ~22min (i5-2540m)
 
 maxcoins = 32
 while True:
@@ -12,15 +12,14 @@ while True:
     for pile1 in range(1,maxcoins):
         for i in range(pile1,maxcoins+1):
             ways[i] += ways[i-pile1]
+            ways[i] %= divby # save modulus value since only need divisibility
     # find a coin set size that is a multiple of the required number
     solution = -1
     for i,w in enumerate(ways):
-        if w % divby == 0:
+        if w == 0:
             solution = i
             break
     if solution != -1:
-        print(': coin set size', solution, 'can be partitioned in',
-              ways[solution], 'ways')
         print(solution)
         break
     maxcoins *= 2
