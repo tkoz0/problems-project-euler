94705f538978d659f0fc1bd9e861d4a27db8c0ba
Your Name <you@example.com>
2018-06-06 22:45:19 -0400

    p074 improved brute force with a cache

--- p074a.py
+++ p074a.py
@@ -2,6 +2,7 @@ import math
 
 startlim = 1000000
 chainlen = 60
+shownums = False # toggle to see satisfying numbers in output
 
 def next(n): # computes next in chain
     s = 0 # use division since its faster than string conversion stuff
@@ -10,15 +11,33 @@ def next(n): # computes next in chain
         n //= 10
     return s
 
-# compute each chain by brute force
+# compute each chain by brute force, with cache it takes ~40sec (i5-2540m)
+# use a cache for finding cycles and solving larger start numbers faster
+cache = [0] * startlim
+cache[0] = 2 # numbers that repeat theirselves (and trivial 0)
+cache[1] = 1
+cache[2] = 1
+cache[145] = 1
+cache[40585] = 1 # from problem 34
+# the only 3 existing cycles (given)
+cache[169] = cache[363601] = cache[1454] = 3
+cache[871] = cache[45631] = 2
+cache[872] = cache[45632] = 2
 
 count = 0
-for n in range(startlim):
-    chain = [n]
-    nn = n
-    while True:
-        nn = next(nn)
-        if nn in chain: break # no repeating numbers
-        chain.append(nn)
-    if len(chain) == chainlen: count += 1
+for n in range(3, startlim):
+    if cache[n] != 0: # already known, determine counting by chain length
+        if cache[n] == chainlen: count += 1
+    else: # must find chain length
+        nn = n
+        ll = 1
+        while True:
+            nn = next(nn)
+            if nn < startlim and cache[nn] != 0: # known subchain length
+                ll += cache[nn]
+                break
+            ll += 1 # havent found subchain, continue
+        if ll == chainlen:
+            count += 1
+            if shownums: print(':', n)
 print(count)
