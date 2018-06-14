a64a26df86b8f1e9ef5c64a1ee88c52f6a402af5
Your Name <you@example.com>
2018-06-13 20:45:34 -0400

    p095 optimizations

--- p095a.py
+++ p095a.py
@@ -9,19 +9,37 @@ for d in range(2, 1+maxnum//2):
         cache[i] += d
 print(': generated divisor sum cache')
 
+# mark numbers visited, longest chain will be found when the smallest in it is
+# visited first time, when exceeding maxnum or reaching 1, chain doesnt work
+visited = [False] * (maxnum+1)
+visited[0] = visited[1] = True
+
 # try chains until either exceeding maxnum or reaching 1
+# takes ~5.5 sec (i5-2540m)
 longest = 0
 minnum = 0
 for n in range(2, maxnum+1):
+    if visited[n]: continue
     start = n
     nums = [n]
+    visited[n] = True
     n = cache[n]
-    while n != 1 and n <= maxnum and not (n in nums): # find numbers in chain
+    while n <= maxnum and not visited[n]: # find numbers in chain
         nums.append(n)
+        visited[n] = True
         n = cache[n]
     nums.append(n)
-    if nums[0] == nums[-1] and len(nums) > longest:
-        longest = len(nums)
-        minnum = min(nums)
-        print(': longer chain', nums)
+    if nums[-1] > maxnum: continue # exceeded limit
+    # last number was visited so see if it exists previously in the chain
+    i = -1
+    for j in range(len(nums)-1):
+        if nums[j] == nums[-1]:
+            i = j
+            break
+    if i == -1: continue # didnt find number
+    if len(nums)-i > longest: # longer chain
+        longest = len(nums)-i
+        minnum = min(nums[i:])
+        print(': longer chain', nums[i:])
+        print(':   all nums visited to find it', nums)
 print(minnum)
