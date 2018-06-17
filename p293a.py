556262fb816e70eb891f0a47eb4d868d2af17732
Your Name <you@example.com>
2018-06-17 18:30:50 -0400

    sped up with miller rabin test

--- p293a.py
+++ p293a.py
@@ -4,19 +4,19 @@ limit = 10**9
 
 # recursively make numbers with consecutive prime factors, for each find the
 # pseudo fortunate number and keep track of distinct M
-# takes ~5sec (cpython / i5-2540m)
+# takes ~1sec (cpython / i5-2540m) (sped up with miller rabin test)
 
 pfset = set() # pseudo fortunate numbers
 def recurse(n,p): # recurse admissible numbers, p is last prime factor used
     global limit, pfset
     p += 1 # find next prime
-    while not lib.prime(p): p += 1
+    while not lib.miller_rabin_verified(p): p += 1
     n *= p
     while n < limit:
         # find M>1 such than N+M is prime
         M = 2
         while True:
-            if lib.prime(n+M):
+            if lib.miller_rabin_verified(n+M):
                 pfset.add(M)
                 break
             M += 1
