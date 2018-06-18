7574cda4f5e38fe4d381ecd70580beddd4d99ea4
Your Name <you@example.com>
2018-06-18 00:06:33 -0400

    p323 alternative solution

--- p323a.py
+++ p323a.py
@@ -25,3 +25,36 @@ while True:
 # round by integer conversion, 10 least significant digits after decimal point
 expected = round(expected * 10**precision)
 print(expected//(10**precision),'.',expected%(10**precision),sep='')
+
+# suppose each bit is a random variable B and there are 32 of them
+# B_i is how many ors were needed to make the ith bit 1
+# for up to k steps to make it a 1, P(B_i<=k)=1-2^(-k)
+# before Z=max(all B_i) so P(Z<=k)=P(B_i<=k)*... since all B are independent
+# so P(Z<=k)=(1-2^(-k))^N (N bits total)
+# P(Z=k) = P(Z<=k) - P(Z<=k-1)
+# now compute E(Z)= sum k*P(Z=k) from k=1 to infinity
+# by using some algebra and combinatorics and summations... (a bit complicated)
+# this evaluates to -sum (N choose i) * (-1)^i / (1-2^(-i)) for i=1 to N
+
+# unfortunately summing directly doesnt give the right answer because
+# floating point math is garbage
+print(':',-sum(lib.binom_coeff(bits,i)*((-1)**i)/(1-(2**(-i))) \
+               for i in range(1,bits+1)))
+
+# compute the fraction and do the division at the end
+N, D = 0, 1
+for i in range(1,bits+1):
+    n = lib.binom_coeff(bits,i) * ((-1)**i) * (2**i)
+    d = (2**i) - 1
+    # N/D + n/d = (Nd+nD)/(Dd)
+    N, D = N*d + n*D, D*d
+    g = lib.gcd_euclid(abs(N),abs(D))
+    N, D = N//g, D//g
+print(':',N) # the fraction result
+print(':',D)
+
+expected = (-N)/D # decimal approximation needed for answer
+
+# output similar to above with 10 decimal places
+expected = round(expected * 10**precision)
+print(expected//(10**precision),'.',expected%(10**precision),sep='')
