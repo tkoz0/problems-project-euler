46297001cc48977feb8d971e2982ead2504a2bdf
Your Name <you@example.com>
2018-06-18 01:52:30 -0400

    p250 solution

---
+++ p250a.py
@@ -0,0 +1,22 @@
+
+nummax = 250250
+divisibility = 250
+modulus = 10**16 # for solution
+
+# building sets 1 element at a time, ~40sec (cpython / i5-2540m)
+
+setswithmod = [0] * divisibility
+setswithmod[0] = 1 # empty set
+
+for n in range(1,nummax+1):
+    mod = pow(n,n,divisibility) # mod residue of next element of the set
+#    print(': next element mod =',mod)
+    swmtmp = setswithmod[:] # amount for each existing subset
+    # for each existing subset, add mod to it and count it
+    for m in range(divisibility):
+        swmtmp[(m+mod)%divisibility] += setswithmod[m]
+        swmtmp[(m+mod)%divisibility] %= modulus
+    setswithmod = swmtmp
+#    print(setswithmod)
+    assert (sum(setswithmod) % modulus) == pow(2,n,modulus)
+print((setswithmod[0]-1)%modulus) # exclude empty set
