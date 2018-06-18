728e3597a7181fa4559fb8908add73c82aa96753
Your Name <you@example.com>
2018-06-18 02:05:27 -0400

    condensed code with a one liner

--- p250a.py
+++ p250a.py
@@ -3,18 +3,17 @@ nummax = 250250
 divisibility = 250
 modulus = 10**16 # for solution
 
-# building sets 1 element at a time, ~40sec (cpython / i5-2540m)
+# building sets 1 element at a time, ~20sec (cpython / i5-2540m)
 
 setswithmod = [0] * divisibility
 setswithmod[0] = 1 # empty set
 
 for n in range(1,nummax+1):
     mod = pow(n,n,divisibility) # mod residue of next element of the set
-    swmtmp = setswithmod[:] # amount for each existing subset
-    # for each existing subset, add mod to it and count it
-    for m in range(divisibility):
-        swmtmp[(m+mod)%divisibility] += setswithmod[m]
-        swmtmp[(m+mod)%divisibility] %= modulus
-    setswithmod = swmtmp
+    # for mod i sets, begin by counting the original
+    # if current element is added to (i-mod)%div sets then we get mod i sets
+    # so count those into the mod i sets as well
+    setswithmod = list((setswithmod[(i-mod)%divisibility]+setswithmod[i])
+                       % modulus for i in range(divisibility))
 #    assert (sum(setswithmod) % modulus) == pow(2,n,modulus)
 print((setswithmod[0]-1)%modulus) # exclude empty set
