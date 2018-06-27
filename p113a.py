5794af27cf92b4f02399e841e9edf8ef98dd1369
Your Name <you@example.com>
2018-06-27 12:26:32 -0400

    p113 simpler solution

--- p113a.py
+++ p113a.py
@@ -16,3 +16,16 @@ maxdigits = 100 # given is count nonbouncies below 10**100
 
 print(sum((lib.binom_coeff(n+9,9) + lib.binom_coeff(n+8,8) - 9 - 1)
           for n in range(1,maxdigits+1)))
+
+# simpler, without summation (consider numbers up to n digits)
+# for increasing, consider all digits and use 9 dividers, if it starts with 0s
+# then it is a number with fewer digits so there are n+9 choose 9 minus 1
+# (the minus 1 is the number with all 0s)
+# for decreasing, use 10 dividers, 0s followed by 9s,8s,...,1s,0s so we consider
+# numbers of varying digits
+# we get a number of all zeroes if the 10 dividers are in the same place which
+# happens n+1 times, in total this has n+10 choose 10 possibilities minus n+1
+# the numbers counted twice are those with same digits so subtract 9*n
+# combining we get (n+9 choose 9) + (n+10 choose 10) - 2 - 10n
+print(lib.binom_coeff(maxdigits+9,9) + lib.binom_coeff(maxdigits+10,10)
+      - 2 - 10*maxdigits)
