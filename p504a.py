a6e5ca1b4714987c7d2892a62bf94dcb81062372
Your Name <you@example.com>
2018-07-06 17:58:11 -0400

    p504 a little faster by precomputing values

--- p504a.py
+++ p504a.py
@@ -7,8 +7,13 @@ m = 100
 # make a set of square numbers for fast checking, maximum is 2m^2
 squares = set(z*z for z in range(2*m))
 
-# gcd table for fast checking
+# gcd table
 gcd = list(list(lib.gcd_euclid(r,c) for c in range(m+1)) for r in range(m+1))
+
+# value table for quick calculation of (a-1)*(b-1) + 1 - gcd[a][b]
+valuet = list(list((a-1)*(b-1)+1-gcd[a][b]
+                   for b in range(m+1))
+              for a in range(m+1))
 
 # brute force all possibilities
 count = 0
@@ -22,11 +27,8 @@ for a in range(1,m+1):
                 points = a+b+c+d - 3 # axis points (origin counted 4 times)
                 # a*b area, exclude points on line, do for each quadrant
                 # these can be shown to be true with pick's theorem
-                ab = (a-1)*(b-1) + 1 - gcd[a][b]
-                bc = (b-1)*(c-1) + 1 - gcd[b][c]
-                cd = (c-1)*(d-1) + 1 - gcd[c][d]
-                da = (d-1)*(a-1) + 1 - gcd[d][a]
-                #assert ab%2==0 and bc%2==0 and cd%2==0 and da%2==0
-                points += (ab+bc+cd+da)//2
+                # the values are generated in the valuet table above
+                points += (valuet[a][b]+valuet[b][c]
+                           +valuet[c][d]+valuet[d][a])//2
                 if points in squares: count += 1
 print(count)
