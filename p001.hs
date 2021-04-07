89577b161e2a340b093a07ea90d5f9ac5e6205d3
TKoz Zero <tkozlowski1999@gmail.com>
2021-04-07 02:30:45 -0400

    (foldl (+) 0) is the same as sum

--- p001.hs
+++ p001.hs
@@ -3,7 +3,7 @@ div3or5 :: Integer -> Bool
 div3or5 i = i `mod` 3 == 0 || i `mod` 5 == 0
 
 compute :: Integer -> Integer
-compute l = foldl (+) 0 (filter div3or5 [1..l-1])
+compute l = sum (filter div3or5 [1..l-1])
 
 main = do
     putStrLn (show (compute 1000))
