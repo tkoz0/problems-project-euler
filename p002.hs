b0f16c05c6ccf3abf978f1b42e6bed60bf2c8e76
TKoz Zero <tkozlowski1999@gmail.com>
2021-04-07 06:23:28 -0400

    wrote a better solution simplifying things

--- p002.hs
+++ p002.hs
@@ -1,20 +1,22 @@
 
--- generates list of fibonacci numbers in reverse order
-fibs :: Integer -> [Integer] -> [Integer]
-fibs l [] = fibs l [1,0]
-fibs l (h:t) = if t == [] then fibs l [1,0]
-    else if h + (head t) > l then [h]++t
-    else fibs l ([h + (head t)] ++ [h] ++ t)
+-- infinite list of fibonacci terms helper function
+-- a,b form the last 2 terms, use first as head and move forward to b,a+b
+fib_helper :: Integer -> Integer -> [Integer]
+fib_helper a b = a : fib_helper b (a+b)
 
--- from fibonacci list, filter to get evens, then sum them
+-- infinite list of fibonacci terms
+fibs :: [Integer]
+fibs = fib_helper 0 1 -- start terms are 0,1
+
+-- simple solution, get numbers up to limit, keep evens, compute sum
 compute :: Integer -> Integer
-compute l = foldl (+) 0 (filter (\x -> x `mod` 2 == 0) (fibs l []))
+compute l = sum $ filter even $ takeWhile (<l) fibs
 
 -- alternative as 1 function: limit, a,b, sum where a,b are fibonacci terms
 compute2 :: Integer -> Integer -> Integer -> Integer -> Integer
 compute2 l a b s =
     if a+b > l then s
-    else if (a+b) `mod` 2 == 0 then compute2 l (max a b) (a+b) (s+a+b)
+    else if even (a+b) then compute2 l (max a b) (a+b) (s+a+b)
     else compute2 l (max a b) (a+b) s
 
 main = do
