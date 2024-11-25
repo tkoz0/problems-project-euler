744d327b5c1a7a9899aacb8647ad3ae3d8069d4c
tkoz0 <13438573+tkoz0@users.noreply.github.com>
2024-11-25 05:31:25 -0500

    analysis of a(n) was incorrect

--- p918a.py
+++ p918a.py
@@ -3,8 +3,13 @@
 # a(2*n) = 2*a(n)
 # a(2*n+1) = a(n) - 3*a(n+1)
 
-# O(log(n)) time to compute a(n)
+# worst case O(n) time, but fast enough for the given value 10^12
+# probably related to the hamming weight of the input
+# particularly bad case example: 2^0 + 2^2 + 2^4 + 2^6 + ... + 2^(2*n)
+num_calls = 0
 def a(n):
+    global num_calls
+    num_calls += 1
     if n == 1: return 1
     n,r = divmod(n,2)
     if r == 0:
@@ -21,7 +26,10 @@ def a(n):
 # = 4 - a(N//2)
 
 N = 10**12
+#N = sum(2**z for z in range(0,32,2))
+#import random; N = random.randint(2**30,2**31-1)
 ans = 4 - a(N//2)
 if N % 2 == 1:
     ans += a(N)
+print(f': a(n) called {num_calls} timies')
 print(ans)
