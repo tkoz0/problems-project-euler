16576ec8036de0bb2f6c8d8f762816443cbed4f1
tkoz0 <13438573+tkoz0@users.noreply.github.com>
2024-11-26 05:57:56 -0500

    logarithmic time analysis

--- p918a.py
+++ p918a.py
@@ -6,7 +6,7 @@
 # worst case O(n) time, but fast enough for the given value 10^12
 # probably related to the hamming weight of the input
 # particularly bad case example: 2^0 + 2^2 + 2^4 + 2^6 + ... + 2^(2*n)
-# but improved with memoization, seems to be O(log(n)) but needs analysis
+# but improved with memoization, to O(log(n)) time
 num_calls = 0
 mem_cache = dict()
 mem_cache[1] = 1
@@ -30,6 +30,29 @@ def a(n):
 # = 1 + 3*a(1) + 3*a(2) + ... + 3*a(N//2-1) + 2*a(N//2)
 #              - 3*a(2) - ... - 3*a(N//2-1) - 3*a(N//2)
 # = 4 - a(N//2)
+
+# analyze the memoized time complexity
+# if n ends in k zeros, we just compute a(n),a(n//2),a(n//4),...,a(n//2**k)
+# if n ends in k ones, think of it as ...011...11
+# (this analysis also works if it is all ones)
+# to compute a(n) we need a(n//2) -> odd, continue
+#                 and a((n+1)//2) -> even and divisible by 2**k
+# then to compute a(n//2) we need a(n//4) -> odd, continue
+#                         and a((n+1)//4) -> even, computed for a((n+1)//2)
+# ...
+# continues to a(n//2**k) (even)
+#          and a((n+1)//2**k) (odd)
+# from here, a(n//2**k) depends on a(n//2**(k+1))
+# a((n+1)//2**k) depends on a(n//2**(k+1))
+#                       and a(n//2**(k+1) + 1)
+# from here, use the following which applies to the odd case above
+# if we depend on n,n+1 then
+# if n is even, we need n//2,n//2+1
+# if n is odd, we need (n-1)//2,(n+1)//2
+# in both cases, we depend on consecutive integers
+# this continues down to bottom and can be used to show the limits
+# a(n) is memoized for <= 2*(1+ceil(log2(n))) values
+# a(n) is called <= 3*(1+ceil(log2(n))) times
 
 N = 10**12
 #N = sum(2**z for z in range(0,32,2))
