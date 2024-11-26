9cb6808dea20e2ee04171d062d2fda6751bbe8f5
tkoz0 <13438573+tkoz0@users.noreply.github.com>
2024-11-26 04:43:59 -0500

    memoization makes it a lot faster

--- p918a.py
+++ p918a.py
@@ -6,16 +6,22 @@
 # worst case O(n) time, but fast enough for the given value 10^12
 # probably related to the hamming weight of the input
 # particularly bad case example: 2^0 + 2^2 + 2^4 + 2^6 + ... + 2^(2*n)
+# but improved with memoization, seems to be O(log(n)) but needs analysis
 num_calls = 0
+mem_cache = dict()
+mem_cache[1] = 1
 def a(n):
-    global num_calls
+    global num_calls,mem_cache
     num_calls += 1
-    if n == 1: return 1
-    n,r = divmod(n,2)
+    if n in mem_cache:
+        return mem_cache[n]
+    nn,r = divmod(n,2)
     if r == 0:
-        return 2*a(n)
+        ret = 2*a(nn)
     else:
-        return a(n) - 3*a(n+1)
+        ret = a(nn) - 3*a(nn+1)
+    mem_cache[n] = ret
+    return ret
 
 # analyze the series
 # a(1) + a(2) + a(3) + ... + a(N) (where N is even)
@@ -27,9 +33,10 @@ def a(n):
 
 N = 10**12
 #N = sum(2**z for z in range(0,32,2))
-#import random; N = random.randint(2**30,2**31-1)
+#import random; N = random.randint(2**255,2**256-1)
 ans = 4 - a(N//2)
 if N % 2 == 1:
     ans += a(N)
-print(f': a(n) called {num_calls} timies')
+print(f': a(n) called {num_calls} times')
+print(f': a(n) cached {len(mem_cache)} times')
 print(ans)
